7ba696e699a86eaed0daf378cd2c11ec285c7f79
Apple-Kong <kongthn@gmail.com>
2022-06-03 15:32:56 +0900

    initial commit with gitignore

---
+++ POMPOM/POMPOM/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  POMPOM
+//
+//  Created by GOngTAE on 2022/06/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, POMPOM")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ POMPOM/POMPOM/POMPOMApp.swift
@@ -0,0 +1,17 @@
+//
+//  POMPOMApp.swift
+//  POMPOM
+//
+//  Created by GOngTAE on 2022/06/03.
+//
+
+import SwiftUI
+
+@main
+struct POMPOMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
