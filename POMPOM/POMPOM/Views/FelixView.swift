6900de9a180d4afcc7ec7852e922dc95a24bf81a
Felix <wonyang22@pos.idserve.net>
2022-06-08 16:25:48 +0900

    [ADD] Felix View를 생성함.

---
+++ POMPOM/POMPOM/Views/FelixView.swift
@@ -0,0 +1,20 @@
+//
+//  FelixView.swift
+//  POMPOM
+//
+//  Created by 양원모 on 2022/06/08.
+//
+
+import SwiftUI
+
+struct FelixView: View {
+    var body: some View {
+        Text("I can fix it!")
+    }
+}
+
+struct FelixView_Previews: PreviewProvider {
+    static var previews: some View {
+        FelixView()
+    }
+}
