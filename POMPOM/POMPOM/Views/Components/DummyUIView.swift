34cf08e7902e88f3701d450e3b8962372b4aa5fa
kongthn <kongthn@gmail.com>
2022-06-08 15:45:10 +0900

    components

---
+++ POMPOM/POMPOM/Views/Components/DummyUIView.swift
@@ -0,0 +1,20 @@
+//
+//  DummyUIView.swift
+//  POMPOM
+//
+//  Created by GOngTAE on 2022/06/08.
+//
+
+import SwiftUI
+
+struct DummyUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DummyUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        DummyUIView()
+    }
+}
