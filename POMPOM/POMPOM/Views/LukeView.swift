cd92badbbbb42189e5db5ae90512ebcc0f61ac2d
kongthn <kongthn@gmail.com>
2022-06-08 16:28:16 +0900

    [ADD] LukeView 생성

---
+++ POMPOM/POMPOM/Views/LukeView.swift
@@ -0,0 +1,20 @@
+//
+//  LukeView.swift
+//  POMPOM
+//
+//  Created by GOngTAE on 2022/06/08.
+//
+
+import SwiftUI
+
+struct LukeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LukeView_Previews: PreviewProvider {
+    static var previews: some View {
+        LukeView()
+    }
+}
