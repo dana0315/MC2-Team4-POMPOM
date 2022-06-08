47a19341e034827afb4f8aa233c6f7d5cdc88533
J2in <jiin62622@gmail.com>
2022-06-08 16:23:00 +0900

    [ADD] create jenibranch
    create jeni branch and add file JeniView2

---
+++ POMPOM/POMPOM/Views/JeniView2.swift
@@ -0,0 +1,20 @@
+//
+//  JeniView2.swift
+//  POMPOM
+//
+//  Created by jiin on 2022/06/08.
+//
+
+import SwiftUI
+
+struct JeniView2: View {
+    var body: some View {
+        Text("SECOND")
+    }
+}
+
+struct JeniView2_Previews: PreviewProvider {
+    static var previews: some View {
+        JeniView2()
+    }
+}
