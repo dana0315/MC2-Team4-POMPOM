b80bbb5d1c308056d86a6204457f370ca9ea2c9a
Felix <wonyang22@pos.idserve.net>
2022-06-10 14:28:39 +0900

    [ADD] OnboardingView - 온보딩의 세번째 페이지를 구성

--- POMPOM/POMPOM/UI/Onboarding/OnboardingView.swift
+++ POMPOM/POMPOM/UI/Onboarding/OnboardingView.swift
@@ -12,20 +12,31 @@ struct OnboardingView: View {
         VStack {
             Spacer()
                 .frame(height: 84)
-            Text("코디 너로 결정!")
+            Text("함께 맞춰봐요")
                 .font(.system(size: 30))
                 .bold()
                 .frame(height: 14)
                 .padding()
-            Text("내 옷장에 색상을 넣고\n같이 컬러 조합을 맞춰봐요")
+            Text("상대방과 연결을 통해\n실시간으로 소통하며 확인해요")
                 .font(.system(size: 17))
                 .multilineTextAlignment(.center)
             Spacer()
                 .frame(height: 75)
-            // "기능 사진" 자리
+            // "공유 사진" 자리
             Rectangle()
                 .frame(width: 350, height: 350)
             Spacer()
+            Button(action: {
+                //임시코드
+                print("시작하기")
+            }) {
+                Text("시작하기")
+                    .padding()
+                    .frame(width: 302, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(8)
+            }
          }
      }
 }
@@ -61,6 +72,7 @@ struct FirstOnboardingView: View {
      }
 }
 
+// "코디 너로 결정!" Onboarding View
 struct SecondOnboardingView: View {
     var body: some View {
         VStack {
@@ -80,6 +92,41 @@ struct SecondOnboardingView: View {
             Rectangle()
                 .frame(width: 350, height: 350)
             Spacer()
+         }
+     }
+}
+
+// "함께 맞춰봐요" Onboarding View
+struct ThirdOnboardingView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+                .frame(height: 84)
+            Text("함께 맞춰봐요")
+                .font(.system(size: 30))
+                .bold()
+                .frame(height: 14)
+                .padding()
+            Text("상대방과 연결을 통해\n실시간으로 소통하며 확인해요")
+                .font(.system(size: 17))
+                .multilineTextAlignment(.center)
+            Spacer()
+                .frame(height: 75)
+            // "공유 사진" 자리
+            Rectangle()
+                .frame(width: 350, height: 350)
+            Spacer()
+            Button(action: {
+                //임시코드
+                print("시작하기")
+            }) {
+                Text("시작하기")
+                    .padding()
+                    .frame(width: 302, height: 50)
+                    .foregroundColor(.white)
+                    .background(Color.black)
+                    .cornerRadius(8)
+            }
          }
      }
 }
