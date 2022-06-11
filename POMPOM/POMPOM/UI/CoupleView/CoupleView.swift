179721cda21f8e6c06cb13db0d79a9cd200a243f
namgeon1106 <namgeon1106@naver.com>
2022-06-12 00:18:09 +0900

    [ADD]: CoupleView.swift - 홈 화면 및 action sheet 구현

--- POMPOM/POMPOM/UI/CoupleView/CoupleView.swift
+++ POMPOM/POMPOM/UI/CoupleView/CoupleView.swift
@@ -8,10 +8,39 @@
 import SwiftUI
 
 struct CoupleView: View {
+    static let characterSpacing = Constant.screenWidth * (33 / 390)
+    static let characterWidth = Constant.screenWidth * (145 / 390)
+    static let characterOffset = Constant.screenHeight * (93 / 844)
+    
+    @State private var partnerConnected = false
+    @State private var actionSheetPresented = false
+    
     var body: some View {
         NavigationView {
             VStack {
-                
+                HStack(spacing: Self.characterSpacing) {
+                    Button {
+                        actionSheetPresented = true
+                    } label: {
+                        ZStack {
+                            Image("Character")
+                                .scaledToFit()
+                                .frame(width: Self.characterWidth)
+                                .opacity(partnerConnected ? 1 : 0.3)
+                            if !partnerConnected {
+                                Text("초대하기")
+                                    .foregroundColor(.orange)
+                            }
+                        }
+                    }
+                    .disabled(partnerConnected)
+
+                    Image("Character")
+                        .scaledToFit()
+                        .frame(width: Self.characterWidth)
+                }
+                .offset(y: Self.characterOffset)
+                Spacer()
             }
             .toolbar {
                 ToolbarItem(placement: .principal) {
@@ -24,6 +53,15 @@ struct CoupleView: View {
                             .foregroundColor(Color(UIColor.label))
                     }
                 }
+            }
+            .actionSheet(isPresented: $actionSheetPresented) {
+                ActionSheet(title: Text("초대코드 확인/입력"), buttons: [
+                    .default(Text("초대코드 확인하기")) {
+                    
+                },
+                    .default(Text("초대코드 입력하기")) {
+                    
+                }, .cancel(Text("돌아가기"))])
             }
         }
     }
