6970e886af47e5e0742d4d14d76f4b3ef48aebf7
namgeon1106 <namgeon1106@naver.com>
2022-06-11 21:18:39 +0900

    [ADD]: Constant 추가 및 screenWidth, screenHeight 프로퍼티 추가

---
+++ POMPOM/POMPOM/Constants/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  POMPOM
+//
+//  Created by 김남건 on 2022/06/11.
+//
+
+import Foundation
+import UIKit
+
+struct Constant {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
