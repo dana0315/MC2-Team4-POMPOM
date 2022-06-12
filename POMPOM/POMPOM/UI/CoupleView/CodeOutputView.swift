7b4dd684a4c6b905e8371e9a71130b9019242612
namgeon1106 <namgeon1106@naver.com>
2022-06-12 15:57:27 +0900

    [ADD]: CodeInputView.swift - 초대코드 입력 화면 구현

--- POMPOM/POMPOM/UI/CoupleView/CodeOutputView.swift
+++ POMPOM/POMPOM/UI/CoupleView/CodeOutputView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct CodeOutputView: View {
+    @Binding var textInput: String
+    let action: () -> ()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        CodeView(title: "초대코드 입력", content: {
+            TextField("", text: _textInput)
+                .padding(.horizontal, 8)
+                .multilineTextAlignment(.center)
+        }, buttonTitle: "확인", buttonAction: action)
     }
 }
 
 struct CodeOutputView_Previews: PreviewProvider {
     static var previews: some View {
-        CodeOutputView()
+        CodeOutputView(textInput: .constant("")) {
+            
+        }
     }
 }
