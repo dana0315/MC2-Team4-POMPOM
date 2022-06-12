337fb8d892d7b25e3a9234f721970aaef049cade
namgeon1106 <namgeon1106@naver.com>
2022-06-12 16:12:46 +0900

    [ADD]: CoupleView.swift - TabView로 화면전환 구현

--- POMPOM/POMPOM/UI/CoupleView/CoupleView.swift
+++ POMPOM/POMPOM/UI/CoupleView/CoupleView.swift
@@ -14,34 +14,49 @@ struct CoupleView: View {
     
     @State private var partnerConnected = false
     @State private var actionSheetPresented = false
+    @State private var selectedTab = "home"
+    @State private var codeInput = ""
     
     var body: some View {
         NavigationView {
-            VStack {
-                HStack(spacing: Self.characterSpacing) {
-                    Button {
-                        actionSheetPresented = true
-                    } label: {
-                        ZStack {
-                            Image("Character")
-                                .scaledToFit()
-                                .frame(width: Self.characterWidth)
-                                .opacity(partnerConnected ? 1 : 0.3)
-                            if !partnerConnected {
-                                Text("초대하기")
-                                    .foregroundColor(.orange)
+            TabView(selection: $selectedTab, content: {
+                VStack {
+                    HStack(spacing: Self.characterSpacing) {
+                        Button {
+                            actionSheetPresented = true
+                        } label: {
+                            ZStack {
+                                Image("Character")
+                                    .scaledToFit()
+                                    .frame(width: Self.characterWidth)
+                                    .opacity(partnerConnected ? 1 : 0.3)
+                                if !partnerConnected {
+                                    Text("초대하기")
+                                        .foregroundColor(.orange)
+                                }
                             }
                         }
-                    }
-                    .disabled(partnerConnected)
+                        .disabled(partnerConnected)
 
-                    Image("Character")
-                        .scaledToFit()
-                        .frame(width: Self.characterWidth)
+                        Image("Character")
+                            .scaledToFit()
+                            .frame(width: Self.characterWidth)
+                    }
+                    .offset(y: Self.characterOffset)
+                    Spacer()
                 }
-                .offset(y: Self.characterOffset)
-                Spacer()
-            }
+                .tag("home")
+                
+                CodeOutputView(code: "ABCSDDFS") {
+                    
+                }
+                .tag("codeOutput")
+                
+                CodeInputView(textInput: $codeInput) {
+                    
+                }
+                .tag("codeInput")
+            })
             .toolbar {
                 ToolbarItem(placement: .principal) {
                     Text("POMPOM")
@@ -57,10 +72,11 @@ struct CoupleView: View {
             .actionSheet(isPresented: $actionSheetPresented) {
                 ActionSheet(title: Text("초대코드 확인/입력"), buttons: [
                     .default(Text("초대코드 확인하기")) {
-                    
+                    selectedTab = "codeOutput"
                 },
                     .default(Text("초대코드 입력하기")) {
-                    
+                    selectedTab = "codeInput"
+                    codeInput = ""
                 }, .cancel(Text("돌아가기"))])
             }
         }
