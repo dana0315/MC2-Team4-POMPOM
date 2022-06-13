dbd7fd6128b3e4d895a6db36c10d5c36b39ad1df
kongthn <kongthn@gmail.com>
2022-06-13 21:00:13 +0900

    [FIX] AppDelegate 구글로그인 부분 삭제

--- POMPOM/POMPOM/System/POMPOMApp.swift
+++ POMPOM/POMPOM/System/POMPOMApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 import FirebaseCore
 import FirebaseAuth
-import GoogleSignIn
+
 
 
 class AppDelegate: NSObject, UIApplicationDelegate {
@@ -20,14 +20,6 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             print("You're sign in as \(user.uid), email: \(user.email ?? "no email")")
         }
         return true
-    }
-    
-    // 인증 절차의 마지막에 받은 URL을 처리하기 위해서 필요한 메서드입니다.
-    @available(iOS 9.0, *)
-    func application(_ application: UIApplication, open url: URL,
-                     options: [UIApplication.OpenURLOptionsKey: Any])
-      -> Bool {
-      return GIDSignIn.sharedInstance.handle(url)
     }
 }
 
