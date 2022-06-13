0ec343877d2d9ead2020eb0e0f31538ea3975c0c
Felix <wonyang22@pos.idserve.net>
2022-06-13 16:33:24 +0900

    [ADD] OnboardingViewModel -  변수와 내용을 추가함. [FIX] OnboardingView - 3개의 뷰를 하나로 통일 시킴. [FIX] Assets - "ChooseClothes"의 파일에 한글로 인한 오류가 생겨 수정함.

--- POMPOM/POMPOM/UI/Onboarding/OnboardingView.swift
+++ POMPOM/POMPOM/UI/Onboarding/OnboardingView.swift
@@ -16,9 +16,9 @@ struct OnboardingView: View {
     
     var body: some View {
         TabView {
-            FirstOnboardingView()
-            SecondOnboardingView()
-            ThirdOnboardingView()
+            OnboardingPageView(onboardingViewModel: onboardingViewData[0])
+            OnboardingPageView(onboardingViewModel: onboardingViewData[1])
+            OnboardingPageView(onboardingViewModel: onboardingViewData[2])
         }
         .tabViewStyle(PageTabViewStyle())
         .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
@@ -32,127 +32,24 @@ struct OnboardingView_Previews: PreviewProvider {
     }
 }
 
-/// "내일 뭐 입고 올거야?" Onboarding View
-struct FirstOnboardingView: View {
-    var body: some View {
-        VStack {
-            Spacer()
-                .frame(height: 84)
-            Text("내일 뭐 입고 올꺼야?")
-                .font(.system(size: 30, weight: .heavy))
-                .frame(height: 20)
-                .padding(17)
-            Text("커플 인생샷을 남기고 싶은데\n상대방이 무엇이 입고올지 고민인가요?")
-                .font(.system(size: 17))
-                .multilineTextAlignment(.center)
-                .lineSpacing(6)
-            Spacer()
-                .frame(height: 80)
-            HStack {
-                Spacer()
-                    .frame(width: 110)
-                Image("CuriousBear")
-                Spacer()
-            }
-            Spacer()
-         }
-     }
-}
-
-/// "코디 너로 결정!" Onboarding View
-struct SecondOnboardingView: View {
-    var body: some View {
-        VStack {
-            Spacer()
-                .frame(height: 84)
-            Text("코디 너로 결정!")
-                .font(.system(size: 30,  weight: .heavy))
-                .frame(height: 20)
-                .padding(17)
-            Text("내 옷장에 색상을 넣고\n같이 컬러 조합을 맞춰봐요")
-                .font(.system(size: 17))
-                .multilineTextAlignment(.center)
-                .lineSpacing(6)
-            Spacer()
-                .frame(height: 75)
-            HStack {
-                Spacer()
-                    .frame(width: 5)
-                Image("ChooseClothes")
-                Spacer()
-            }
-            
-            Spacer()
-         }
-     }
-}
-
-/// "함께 맞춰봐요" Onboarding View
-struct ThirdOnboardingView: View {
-    var body: some View {
-        VStack {
-            Spacer()
-                .frame(height: 84)
-            Text("함께 맞춰봐요")
-                .font(.system(size: 30, weight: .heavy))
-                .frame(height: 20)
-                .padding(17)
-            Text("상대방과 연결을 통해\n실시간으로 소통하며 확인해요")
-                .font(.system(size: 17))
-                .multilineTextAlignment(.center)
-                .lineSpacing(6)
-            Spacer()
-                .frame(height: 75)
-            HStack {
-                Spacer()
-                Image("Connection")
-                Spacer()
-            }
-            Spacer()
-            Button(action: {
-                //임시코드 - 누르면 CoupleView로 넘어감.
-                print("시작하기")
-                // isFirstLaunching.toggle()
-                // 위에 코드로 변경예정
-                
-            }) {
-                Text("시작하기")
-                    .padding()
-                    .frame(width: 302, height: 50)
-                    .foregroundColor(.white)
-                    .background(Color.black)
-                    .cornerRadius(8)
-            }
-            .padding(20)
-            Spacer()
-                .frame(height: 50)
-         }
-     }
-}
-
-//한개의 뷰로 구현
+///한개의 뷰로 구현
 struct OnboardingPageView: View {
-    var title: String
-    var message: String
-    var onboardingImage: String
-    var ImageSpacer: CGFloat
+    var onboardingViewModel: OnboardingViewModel
     
-    init(title: String, message: String, onbordingImage: String, ImageSpacer: CGFloat) {
-        self.title = title
-        self.message = message
-        self.onboardingImage = onbordingImage
-        self.ImageSpacer = ImageSpacer
+    
+    init(onboardingViewModel: OnboardingViewModel) {
+        self.onboardingViewModel = onboardingViewModel
     }
     
     var body: some View {
         VStack {
             Spacer()
                 .frame(height: 84)
-            Text(title)
+            Text(onboardingViewModel.title)
                 .font(.system(size: 30, weight: .heavy))
                 .frame(height: 20)
                 .padding(17)
-            Text(message)
+            Text(onboardingViewModel.message)
                 .font(.system(size: 17))
                 .multilineTextAlignment(.center)
                 .lineSpacing(6)
@@ -160,8 +57,8 @@ struct OnboardingPageView: View {
                 .frame(height: 80)
             HStack {
                 Spacer()
-                    .frame(width: ImageSpacer)
-                Image(onboardingImage)
+                    .frame(width: onboardingViewModel.ImageSpacer)
+                Image(onboardingViewModel.onboardingImage)
                 Spacer()
             }
             Spacer()
--- POMPOM/POMPOM/UI/Onboarding/OnboardingViewModel.swift
+++ POMPOM/POMPOM/UI/Onboarding/OnboardingViewModel.swift
@@ -5,4 +5,36 @@
 //  Created by 양원모 on 2022/06/13.
 //
 
-import Foundation
+import SwiftUI
+
+struct OnboardingViewModel: Identifiable {
+    var id = UUID()
+    var title: String
+    var message: String
+    var onboardingImage: String
+    var ImageSpacer: CGFloat
+}
+
+let onboardingViewData : [OnboardingViewModel] = [
+    OnboardingViewModel(
+        title: "내일 뭐 입고 올꺼야?",
+        message: "커플 인생샷을 남기고 싶은데\n상대방이 무엇이 입고올지 고민인가요?",
+        onboardingImage: "CuriousBear",
+        ImageSpacer: 110
+    ),
+    
+    OnboardingViewModel(
+        title: "코디 너로 결정!",
+        message: "내 옷장에 색상을 넣고\n같이 컬러 조합을 맞춰봐요",
+        onboardingImage: "ChooseClothes",
+        ImageSpacer: 6
+    ),
+    
+    OnboardingViewModel(
+        title: "함께 맞춰봐요",
+        message: "상대방과 연결을 통해\n실시간으로 소통하며 확인해요",
+        onboardingImage: "Connection",
+        ImageSpacer: 36
+    )
+]
+
