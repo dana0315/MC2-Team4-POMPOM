1872f5e8a80112e570e0ccbedf24f5f789331ec1
kongthn <kongthn@gmail.com>
2022-06-14 11:19:52 +0900

    [ADD] Category 선택 grid 구현

--- POMPOM/POMPOM/UI/ClothPickerView/ClothPickerView.swift
+++ POMPOM/POMPOM/UI/ClothPickerView/ClothPickerView.swift
@@ -8,27 +8,31 @@
 import SwiftUI
 
 struct ClothPickerView: View {
-    @StateObject var viewModel = ViewModel()
- 
+    @StateObject var viewModel = PickerViewModel()
+    
+    
     var body: some View {
         ScrollView(showsIndicators: false) {
             VStack {
+                CategoryGrid(viewModel: viewModel)
+                
+                Seperator()
+                
                 HStack {
                     Text("내 옷장")
-                        .font(.custom("몰라", size: 15))
+                        .font(.custom("none", size: 15))
                     Spacer()
                 }
                 .padding(.leading, 20)
-                .padding(.bottom, 10)
+                
                 
                 ScrollView(.horizontal, showsIndicators: false) {
                     ColorGrid(viewModel: viewModel)
-                    .padding(.leading, 20)
-                    .padding(.bottom, 20)
+                        .padding(.leading, 10)
+                        .padding(10)
+                    
                 }
-               Rectangle()
-                    .fill(.black.opacity(0.3))
-                    .frame(height: 0.75)
+                Seperator()
                     .padding(.bottom, 20)
                 ClothGrid(viewModel: viewModel)
                     .padding(.horizontal, 20)
@@ -37,25 +41,52 @@ struct ClothPickerView: View {
     }
 }
 
+struct CategoryGrid: View {
+    @ObservedObject var viewModel: PickerViewModel
+    @State var cuurentCategory = ClothCategory.hat
+    
+    let rows = [GridItem(.fixed(44), spacing: 20)]
+    var body: some View {
+        LazyHGrid(rows: rows, spacing: 27) {
+            ForEach(ClothCategory.allCases) { category in
+                Text(category.koreanSubtitle())
+                    .font(.body)
+                    .fontWeight(.semibold)
+                    .foregroundColor(category == cuurentCategory ? Color(hex: "FF5100") : Color(hex: "A0A0A0"))
+                    .onTapGesture {
+                        // 맥락 바꾸기.
+                        cuurentCategory = category
+                        
+                    }
+            }
+        }
+    }
+}
+
 struct ColorGrid: View {
-    @ObservedObject var viewModel: ViewModel
+    @ObservedObject var viewModel: PickerViewModel
     let rows = [GridItem(.fixed(44), spacing: 20)]
     var body: some View {
         LazyHGrid(rows: rows, spacing: 18) {
-            ForEach($viewModel.items) { item in
+            ForEach($viewModel.presets, id: \.self) { item in
                 ZStack {
                     Circle()
-                        .fill(.blue)
+                        .fill(Color(hex: item.wrappedValue))
                         .frame(width: 44)
-                   
+                        .shadow(radius: 5)
                 }
             }
             Circle()
-                .fill(.pink)
+                .fill(Color(hex: "D8D8D8"))
                 .frame(width: 44)
+                .overlay(
+                    Image(systemName: "plus")
+                        .font(.title2)
+                        
+                )
                 .onTapGesture {
                     print("DEBUG : Button tapped")
-                    viewModel.addItem()
+                    viewModel.addPreset(hex: "23F323")
                 }
         }
     }
@@ -63,19 +94,19 @@ struct ColorGrid: View {
 
 
 struct ClothGrid: View {
-    @ObservedObject var viewModel: ViewModel
+    @ObservedObject var viewModel: PickerViewModel
     let columns = [
         GridItem(.flexible(minimum: 60, maximum: 200), spacing: 20),
         GridItem(.flexible(minimum: 60, maximum: 200),spacing: 20)
     ]
     var body: some View {
         LazyVGrid(columns: columns, spacing: 20) {
-            ForEach(viewModel.items) { item in
+            ForEach(0..<12) { item in
                 RoundedRectangle(cornerRadius: 10)
                     .fill(Color.blue)
                     .frame(height: 160)
                     .onTapGesture {
-                        print(item.id)
+                        
                     }
             }
         }
@@ -85,5 +116,13 @@ struct ClothGrid: View {
 struct ClothPickerView_Previews: PreviewProvider {
     static var previews: some View {
         ClothPickerView()
+    }
+}
+
+struct Seperator: View {
+    var body: some View {
+        Rectangle()
+            .fill(Color(hex: "D8D8D8"))
+            .frame(height: 0.75)
     }
 }
--- POMPOM/POMPOM/UI/ClothPickerView/ViewModel/PickerViewModel.swift
+++ POMPOM/POMPOM/UI/ClothPickerView/ViewModel/PickerViewModel.swift
@@ -5,24 +5,76 @@
 //  Created by GOngTAE on 2022/06/13.
 //
 
-import Foundation
+import SwiftUI
 
-class ViewModel: ObservableObject {
-    @Published var items: [Model] = {
-        var result: [Model] = []
-        for i in 0..<12 {
-            result.append(Model(id: i))
-        }
+class PickerViewModel: ObservableObject {
+    @Published var presets: [String] = ["FFFFFF", "121212", "234212"]
+    @Published var items: [Cloth] = {
+        var result: [Cloth] = []
+        
         return result
     }()
     
-    func addItem() {
-        items.append(Model(id: items.count + 1))
-        print("DEBUG: add Item")
+    func addPreset(hex: String) {
+        if presets.contains(hex) {
+            print("DEBUG: 중복된 hexcode preset")
+            return
+        }
+        presets.append(hex)
     }
 }
 
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        scanner.currentIndex = hex.startIndex
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
 
-struct Model: Identifiable {
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff
+        )
+    }
+}
+
+struct Cloth: Identifiable {
     var id: Int
+    var name: String
+    var category: ClothCategory
+}
+
+enum ClothCategory: CaseIterable, Identifiable {
+    var id: ClothCategory {
+        self
+    }
+    
+    case hat
+    case top
+    case layered
+    case bottom
+    case socks
+    case shoes
+    
+    func koreanSubtitle() -> String {
+        switch self {
+        case .hat:
+            return "모자"
+        case .top:
+            return "상의"
+        case .layered:
+            return "레이어드"
+        case .bottom:
+            return "하의"
+        case .socks:
+            return "양말"
+        case .shoes:
+            return "신발"
+        }
+    }
 }
