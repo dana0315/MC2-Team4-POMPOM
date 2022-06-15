c82c953fccf81b69aff810edd4dc4ba3c3ff8bb5
kongthn <kongthn@gmail.com>
2022-06-15 17:03:52 +0900

    [FIX] 카테고리간 spacing 수정, ForcedUnwrap 제거

--- POMPOM/POMPOM/UI/ClothPickerView/CategoryGrid.swift
+++ POMPOM/POMPOM/UI/ClothPickerView/CategoryGrid.swift
@@ -13,6 +13,7 @@ struct CategoryGrid: View {
     @Binding var currentHex: String
     @Binding var currentCategory: ClothCategory
     let rows = [GridItem(.fixed(44))]
+    let spacing: CGFloat = 60
     var body: some View {
         LazyHGrid(rows: rows, spacing: 40) {
             ForEach(ClothCategory.allCases) { category in
@@ -23,19 +24,20 @@ struct CategoryGrid: View {
                     .onTapGesture {
                         // 맥락 바꾸기.
                         vm.changeCategory(with: category)
-                        currentHex = vm.currentPresets.first!
+                        currentHex = vm.currentPresets.first ?? "FFFFFF"
+                        
                         withAnimation(.spring()) {
                             switch vm.currentType {
                             case .hat:
-                                offSet = Constant.screenWidth / 2 - 60
+                                offSet = Constant.screenWidth / 2 - spacing
                             case .top:
-                                offSet = Constant.screenWidth / 2 - 120
+                                offSet = Constant.screenWidth / 2 - spacing * 2
                             case .bottom:
-                                offSet = Constant.screenWidth / 2 - 190
+                                offSet = Constant.screenWidth / 2 - spacing * 3
                             case .shoes:
-                                offSet = Constant.screenWidth / 2 - 265
+                                offSet = Constant.screenWidth / 2 - spacing * 4
                             case .accessories:
-                                offSet = Constant.screenWidth / 2 - 330
+                                offSet = Constant.screenWidth / 2 - spacing * 5
                             }
                         }
                         currentCategory = category
