a71a7e5f97b401cb19b34618605968dce22e87b1
Felix <wonyang22@pos.idserve.net>
2022-06-15 14:37:28 +0900

    [ADD] ColorPickerView - "ColorPickerView" 그룹과 파일 생성

---
+++ POMPOM/POMPOM/UI/ColorPickerView/ColorPickerView.swift
@@ -0,0 +1,20 @@
+//
+//  ColorPickerView.swift
+//  POMPOM
+//
+//  Created by 양원모 on 2022/06/15.
+//
+
+import SwiftUI
+
+struct ColorPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorPickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorPickerView()
+    }
+}
